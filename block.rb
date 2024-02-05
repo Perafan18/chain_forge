602fd4ce907f2d356cb44c4d5b8806e3c7bb0769
Pedro Perafan <pedro.perafan.carrasco@gmail.com>
2024-02-05 14:59:30 -0600

    Enhance Block class and add relevant unit tests
    Block class now carries added comments for each method, making code easier to understand. A unit testing file has also been introduced to validate the key functionalities of the Block class such as initialization and hash calculation, ensuring the class functionality matches the expected

--- block.rb
+++ block.rb
@@ -2,24 +2,45 @@
 
 require 'digest'
 
+# The Block class is part of a Blockchain that holds the digital
+# information (the “block”) stored in a public database (the “chain”).
+#
+# Each block has an index, a timestamp (in Unix time), transaction
+# data, a hash pointer to the previous block’s hash, and a hash of its
+# own data.
+# It provides methods for creating and managing the blocks on the chain.
 class Block
+  # Allow read access for instance variables
   attr_reader :index, :data, :timestamp, :previous_hash, :hash
 
+  # Initialize a new Block.
+  #
+  # @param [Integer] index The location of the block on the chain.
+  # @param [Object] data The information that is stored inside the block.
+  # @param [String] previous_hash The hash of the previous block in the chain.
   def initialize(index, data, previous_hash)
     @index = index
-    @timestamp = time_now
+    @timestamp = current_time
     @data = data
     @previous_hash = previous_hash
     @hash = calculate_hash
   end
 
+  # Calculates the SHA256 hash of the block.
+  #
+  # The hash is generated from the block's index, timestamp, transaction data,
+  # and the hash of the previous block.
+  #
+  # @return [String] the hash of the block
   def calculate_hash
     Digest::SHA256.hexdigest("#{@index}#{@timestamp}#{@data}#{@previous_hash}")
   end
 
   private
 
-  def time_now
+  # Get the current unix timestamp
+  # @return [Integer] current unix timestamp
+  def current_time
     Time.now.to_i
   end
 end
---
+++ spec/block_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+#
+require 'rspec'
+require_relative '../block'
+
+RSpec.describe Block do
+  let(:index) { 1 }
+  let(:data) { 'block data' }
+  let(:prev_hash) { 'abcde12345' }
+  let(:now) { Time.now.to_i }
+
+  subject { described_class.new(index, data, prev_hash) }
+
+  it 'initializes with correct attributes' do
+    expect(subject.index).to eq(index)
+    expect(subject.data).to eq(data)
+    expect(subject.previous_hash).to eq(prev_hash)
+  end
+
+  describe '#hash' do
+    it 'returns a SHA256 hash' do
+      expect(subject.hash.size).to eq(64)  #A SHA256 digest string is 64 characters long
+      expect(subject.hash).to_not include(" ")  #A SHA256 digest string has no empty spaces
+    end
+  end
+
+  describe '#timestamp' do
+    it 'returns the current Unix timestamp' do
+      expect(subject.timestamp).to be_within(1).of(now)
+    end
+  end
+end
