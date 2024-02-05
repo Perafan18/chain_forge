7f126c090e0bb3ba55ed5cf2d5ce15bfe3fb5116
Pedro Perafan <pedro.perafan.carrasco@gmail.com>
2024-02-04 23:03:36 -0600

    Add unit tests for Blockchain class
    This commit introduces a set of unit tests for the Blockchain class, covering its various methods such as initialization, adding a new block, validating the blockchain, etc. Additional comments and explanatory notes have also been included in the source code of the original Blockchain class for better readability, and a better understanding of the code workflow.

--- blockchain.rb
+++ blockchain.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
+# This is the 'Blockchain' class which models a simple blockchain system.
+# The Blockchain is an array of Blocks. It contains methods to create an instance
+# of a Blockchain, to add a new block into the chain, to validate the entire chain, etc.
+
 require_relative 'block'
 
 class Blockchain
+  # @return [Array<Block>] the array representation of the blockchain
   attr_reader :chain
 
+  # Initialize the new Blockchain object with Genesis block
+  # Genesis block is added to the blockchain during initilialization
   def initialize
     @chain = [create_genesis_block]
   end
 
+  # This method adds a new block to the blockchain
+  # @param data [Object] The data that needs to be added to the new block
   def add_block(data)
     @chain << new_block(data)
   end
 
-  def new_block(data)
-    Block.new(
-      last_block.index + 1,
-      data,
-      last_block.hash
-    )
-  end
-
+  # This method returns the last block (current tail) of the blockchain
+  # @return [Block] Returns the last block in the chain
   def last_block
     @chain.last
   end
 
+  # This method checks if the blockchain is valid or not
+  # It validates the hash links and recalculates the hashes to check consistency.
+  # @return [Boolean] Returns true if the blockchain is valid, otherwise false.
   def valid?
     @chain.each_cons(2) do |previous_block, current_block|
       return false if previous_block.hash != current_block.previous_hash
@@ -36,6 +42,23 @@ class Blockchain
 
   private
 
+  # This method creates a new block in the blockchain.
+  # It calculates the new block's index, data and previous_hash.
+  # @param data [Object] The data that needs to be added to the new block
+  # @return [Block] Returns a new Block with the passed data and appropriate index, previous_hash
+  def new_block(data)
+    Block.new(
+      last_block.index + 1,
+      data,
+      last_block.hash
+    )
+  end
+
+  # This method creates the Genesis block
+  # The genesis block is added to the blockchain during initialization,
+  # and it is the only block that can have '0' as its previous_hash
+  # @return [Block] Returns the genesis Block object
+  # @raise [RuntimeError] Raises an error if an attempt to create genesis block is found when genesis block already exists in the chain
   def create_genesis_block
     raise 'Genesis Block already exists' unless @chain.nil?
 
---
+++ spec/blockchain_spec.rb
@@ -0,0 +1,40 @@
+# blockchain_spec.rb
+require_relative '../blockchain'  # adjust this path to the correct one for your project
+require_relative '../block'  # adjust this path to the correct one for your project
+
+RSpec.describe Blockchain do
+  subject { Blockchain.new }
+
+  describe '#initialize' do
+    it 'creates a new blockchain with a single block (genesis block)' do
+      expect(subject.chain.length).to eq(1)
+      expect(subject.chain[0].index).to eq(0)
+    end
+  end
+
+  describe '#add_block' do
+    it 'adds a new block to the blockchain' do
+      subject.add_block('Block Data')
+      expect(subject.chain.length).to eq(2)
+    end
+  end
+
+  describe '#last_block' do
+    it 'returns the last block in the chain' do
+      subject.add_block('Block Data')
+      expect(subject.last_block.data).to eq('Block Data')
+    end
+  end
+
+  describe '#valid?' do
+    it 'validates the blockchain' do
+      expect(subject.valid?).to be true
+    end
+
+    it 'invalidates the blockchain if the hash is inconsistent' do
+      subject.add_block('Block Data')
+      subject.last_block.instance_variable_set(:@hash, 'bogus hash')
+      expect(subject.valid?).to be false
+    end
+  end
+end
